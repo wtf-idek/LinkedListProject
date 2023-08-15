91ad786b2d0346d4e6b3ad6ee1ded0b9444feb4e
wtf-idek <142284423+wtf-idek@users.noreply.github.com>
2023-08-14 20:47:21 -0700

    Update P4-Linked-List.cpp

--- P4-Linked-List.cpp
+++ P4-Linked-List.cpp
@@ -54,53 +54,21 @@ int main() {
 	//I'm trying to check each word in one list against the other 
 	//and remove it from the mod list if it's there
 	
-	//JJString check1 = modList1.next();
-	//JJString check2 = list2.next();
+	JJString check1 = modList1.next();
+	JJString check2 = list2.next();
 
-	//this throws an error in my JJString.cpp at the == overload or the != for the isupper
+	//this throws an error in my JJString.cpp at the == overload  for the isupper check for cstr
 	//I think the problem is actually in the remove function of DbleLinkedList.cpp
-	//I am also aware that it doesn't go through and remove every isntance of the word, I'm working on that
-	//for (int i = 0; i <= list2.getCount(); ++i) {
-	//	for (int j = 0; j <= modList1.getCount(); ++j) {
-	//		if (check2 == check1) {
-	//			modList1.remove(check2);
-	//		}
-	//		check1 = modList1.next();
-	//	}
-	//	check2 = list2.next();
-	//	modList1.resetIterator();
-	//}
-
-	//cout << endl;
-	//cout << "There are " << list1.getCount() << " words on List 1." << endl;
-	//cout << "There are " << list2.getCount() << " words on list 2." << endl;
-	//cout << "There are " << modList1.getCount() << " words on Modified List 1." << endl;
-	//cout << "There are " << modList2.getCount() << " words on Modified list 2." << endl;
-
-	//list1.resetIterator();
-	//list2.resetIterator();
-	//modList1.resetIterator();
-	//modList2.resetIterator();
-
-	//check1 = modList2.next();
-	//check2 = list1.next();
-
-	//I also tried this way. It actually seems to run but very slowly. I think there's a better way.
-	//it also doesn't go through eact item
-	//while (modList2.hasMore()) {
-	//	while (list1.hasMore()) {
-	//		modList2.remove(check2);
-	//		check2 = list1.next();
-	//	}
-	//	check1 = modList2.next();
-	//	list1.resetIterator();
-	//}
-
-	//cout << endl;
-	//cout << "There are " << list1.getCount() << " words on List 1." << endl;
-	//cout << "There are " << list2.getCount() << " words on list 2." << endl;
-	//cout << "There are " << modList1.getCount() << " words on Modified List 1." << endl;
-	//cout << "There are " << modList2.getCount() << " words on Modified list 2." << endl;
+		while (modList1.hasMore()) {
+		for (int i = 0; i <= list2.getCount(); ++i) {
+			if (check1 == check2) {
+				modList1.remove(check2);
+			}
+			check2 = list2.next();
+		}
+		list2.resetIterator();
+		check1 = modList1.next();
+	}
 
 
 	system("pause");
